981a584dfa769c53367019e702845a1d21c7213d
Caleb <calebelizondo@tamu.edu>
2023-10-01 00:57:26 +0000

    added GPA and grade dist to professor query

--- grade_dist_API/grade_dist_api_app/views.py
+++ grade_dist_API/grade_dist_api_app/views.py
@@ -6,16 +6,62 @@ from grade_dist_api_app.models import Section
 # Create your views here.
 from django.http import HttpResponse
 
+grading_system = {'a': 4.0, 'b': 3.0, 'c': 2.0, 'd': 1.0, 'f': 0.0}
+
 def home(request):
     return render(request, 'display.html')
 
 
-def get_profs(request, subject_code, course_code):
-    professors = Section.objects.filter(subject_code=subject_code.upper(), course_code=course_code).values_list('professor_name', flat=True).distinct()
+def calculate_gpa(grade_counts):
+    # Define your grading system here (e.g., A=4.0, B=3.0, etc.)
 
-    professor_data = [{'professor': prof} for prof in professors]
+    total_points = 0
+    total_students = 0
+
+    for grade, count in grade_counts.items():
+        total_points += grading_system.get(grade, 0) * count
+        total_students += count
+
+    if total_students == 0:
+        return 0.0
+
+    return total_points / total_students
+
+def get_profs(request, subject_code, course_code):
+    subject_code = subject_code.upper()
+    course_code = course_code.upper()
+
+    # Filter the SectionGrade objects based on subject code and course code
+    section_grades = Section.objects.filter(subject_code=subject_code, course_code=course_code)
+
+    professor_data = []
+
+    # Group section grades by professor name
+    professors = set(section_grade.professor_name for section_grade in section_grades)
+
+    for professor in professors:
+        professor_grades = section_grades.filter(professor_name=professor)
+        print(professor_grades)
+
+        # Calculate the grade distribution for the professor
+        grade_distribution = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'f': 0}
+        for section_grade in professor_grades:
+            for grade in grading_system.keys():
+                grade_distribution[grade] += getattr(section_grade, "{}_count".format(grade.lower()), 0)
+
+        # Calculate the average GPA for the professor
+        average_gpa = calculate_gpa(grade_distribution)
+
+        professor_info = {
+            'professor': professor,
+            'average_gpa': average_gpa,
+            'grade_distribution': grade_distribution
+        }
+
+        professor_data.append(professor_info)
 
     return JsonResponse(professor_data, safe=False)
+
 
 def get_subject_codes(request):
 
